a9a0646d5e3b8beb3d7df438a44aa09777f5d514
JeremyDallain <jdallain87@gmail.com>
2023-05-01 18:11:03 +0200

    add github link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,16 @@
 import {IoLogoLinkedin} from 'react-icons/io';
+import {IoLogoGithub} from 'react-icons/io';
+
 
 export const Footer = () => {
   return (
-    <footer className="h-12 flex justify-center items-center relative mt-6 text-blue-700">
+    <footer className="h-12 flex justify-center items-center mt-6 text-blue-700">
       <p className="text-sm sm:text-lg text-center font-medium">Site Réalisé par Jérémy Dallain</p>
       <a href="https://www.linkedin.com/in/jeremy-dallain/" title='profil de Jérémy Dallain sur linkedin' >
-        <IoLogoLinkedin size={30} className='absolute top-2 ml-5 ' />
+        <IoLogoLinkedin size={30} className='top-2 ml-5 ' />
+      </a>
+      <a href="https://github.com/JeremyDallain/notes-in-localstorage" title='depôt github du projet' >
+        <IoLogoGithub size={30} className='top-2 ml-5 ' />
       </a>
     </footer>
   );
